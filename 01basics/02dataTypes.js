f856f2758f10b5d6de36e7a5d1a378c9357c565f
Ajeet Varman <148537325+ajeetvarman@users.noreply.github.com>
2025-05-23 06:52:09 +0000

    variable

---
+++ 01basics/02dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log( 3 + 3 ) // code readability should be high
+
+console.log(" Ajeet ")
+
+let name = "Ajeet"
+let age = 24
+let isLoggedin = false
+let state = null
+
+/*
+number => 2 to power 53
+bigint 
+string => ""
+boolean => true/false
+indefined => no value 
+null => empty value
+symbol => unique value
+object => non-primitive data type
+*/
+
+console.log(typeof "Ajeet") // string
+
+console.log(typeof null) // object 
+
+console.log ( typeof undefined ) //undefined
